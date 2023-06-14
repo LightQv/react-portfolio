26d09e3fbe4967bfb946adbf85d91679aeed1b61
LightQv <QVivian23@gmail.com>
2023-06-14 18:45:25 +0200

    updated components

---
+++ src/data.js
@@ -0,0 +1,63 @@
+import wildfest from "./assets/image/projects/wildfest.png";
+import pokedex from "./assets/image/projects/pokedex.png";
+import keskonregarde from "./assets/image/projects/keskonregarde.png";
+import animalz from "./assets/image/projects/animalz.png";
+import discovery from "./assets/image/projects/discovery.png";
+
+export const projects = [
+  {
+    id: 1,
+    name: "WildFest",
+    img: wildfest,
+    description:
+      "is a website for a music festival that allow the users to check every different artists and prices.",
+    language: "HTML - CSS - JavaScript",
+    deployed: "gh-pages",
+    code_source: "https://github.com/LightQv/Wild-Festival",
+    demo_link:
+      "https://wildcodeschool.github.io/2023-02-JS-FT-Lyon-P1-BrainForge/index.html",
+  },
+  {
+    id: 2,
+    name: "Pokedex",
+    img: pokedex,
+    description: "is an app which lists every Pokemon's datas sorted by areas.",
+    language: "React.js - CSS - Node.js",
+    deployed: "gh-pages",
+    code_source: "https://github.com/LightQv/pokedex",
+    demo_link: "https://lightqv.github.io/pokedex/",
+  },
+  {
+    id: 3,
+    name: "Keskonregarde ?",
+    img: keskonregarde,
+    description:
+      "is a search website that aim to help the users to find a movie to watch.",
+    language: "React.js - CSS - Node.js",
+    deployed: "Netlify",
+    code_source: "https://github.com/LightQv/keskonregarde",
+    demo_link: "https://lightqv-keskonregarde.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Animalz",
+    img: animalz,
+    description:
+      "is a meeting app that help travelers to find a partner. Hackathon 24h proof of concept.",
+    language: "React.js - CSS - Node.js",
+    deployed: null,
+    code_source: "https://github.com/LightQv/animalz",
+    demo_link: null,
+  },
+  {
+    id: 5,
+    name: "Discovery",
+    img: discovery,
+    description:
+      "is a Spotify client to center the most usefull datas from the user's account.",
+    language: "React.js - Tailwind CSS - Express.js",
+    deployed: null,
+    code_source: "https://github.com/LightQv/discovery",
+    demo_link: null,
+  },
+];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,17 +4,12 @@ import { BsWindowPlus } from "react-icons/bs";
 import ProjectCard from "../components/ProjectCard";
 import styles from "./Projects.module.css";
 import DetailsModal from "../components/DetailsModal";
+import { projects } from "../data";
+import ProjectDescription from "../components/ProjectDescription";
 
 export default function Projects() {
-  const [cardIsActive, setCardIsActive] = useState(false);
-  const [wildIsActive, setWildIsActive] = useState(false);
-  const [pokedexIsActive, setPokedexIsActive] = useState(false);
-  const [keskonregardeIsActive, setKeskonregardeIsActive] = useState(false);
-  const [animalzIsActive, setAnimalzIsActive] = useState(false);
-  const [discoveryIsActive, setDiscoveryIsActive] = useState(false);
+  const [projectIsActive, setProjectIsActive] = useState(null);
   const [modalIsActive, setModalIsActive] = useState(false);
-  const [codeIsHover, setCodeIsHover] = useState(false);
-  const [liveIsHover, setLiveIsHover] = useState(false);
 
   return (
     <main className={styles.projectPage}>
@@ -30,253 +25,36 @@ export default function Projects() {
             BsCodeSlash={BsCodeSlash}
             BsWindowPlus={BsWindowPlus}
             setModalIsActive={setModalIsActive}
+            projects={projects}
           />
         ) : null}
       </section>
       <section className={styles.desktopLayout}>
-        {cardIsActive === false ? (
+        {projectIsActive !== null ? (
+          <ProjectDescription
+            BsCodeSlash={BsCodeSlash}
+            BsWindowPlus={BsWindowPlus}
+            projects={projects}
+            projectIsActive={projectIsActive}
+          />
+        ) : (
           <div className={styles.pageInfos}>
             <h2 className={styles.pageTitle}>Select a project</h2>
             <h4 className={styles.pageSubtitle}>to learn more about it !</h4>
           </div>
-        ) : null}
-        {wildIsActive ? (
-          <div className={styles.projectInfos}>
-            <div>
-              <div className={styles.topSection}>
-                <h2 className={styles.projectTitle}>WildFest</h2>
-                <h4 className={styles.projectStack}>HTML - CSS - JavaScript</h4>
-              </div>
-              <p className={styles.projectDescription}>
-                is a website for a music festival that allow the users to check
-                every different artists and prices.
-              </p>
-            </div>
-            <div className={styles.linkSection}>
-              <a
-                href="https://github.com/LightQv/Wild-Festival"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setCodeIsHover(true)}
-                onMouseLeave={() => setCodeIsHover(false)}
-              >
-                Code
-                <BsCodeSlash
-                  className={styles.projectIcon}
-                  style={
-                    codeIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-              <a
-                href="https://wildcodeschool.github.io/2023-02-JS-FT-Lyon-P1-BrainForge/index.html"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setLiveIsHover(true)}
-                onMouseLeave={() => setLiveIsHover(false)}
-              >
-                Live demo
-                <BsWindowPlus
-                  className={styles.projectIcon}
-                  style={
-                    liveIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-            </div>
-          </div>
-        ) : null}
-        {pokedexIsActive ? (
-          <div className={styles.projectInfos}>
-            <div>
-              <div className={styles.topSection}>
-                <h2 className={styles.projectTitle}>Pokedex</h2>
-                <h4 className={styles.projectStack}>
-                  React.js - CSS - Node.js
-                </h4>
-              </div>
-              <p className={styles.projectDescription}>
-                is an app which lists every Pokemon's datas sorted by areas.
-              </p>
-            </div>
-            <div className={styles.linkSection}>
-              <a
-                href="https://github.com/LightQv/pokedex"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setCodeIsHover(true)}
-                onMouseLeave={() => setCodeIsHover(false)}
-              >
-                Code
-                <BsCodeSlash
-                  className={styles.projectIcon}
-                  style={
-                    codeIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-              <a
-                href="https://lightqv.github.io/pokedex/"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setLiveIsHover(true)}
-                onMouseLeave={() => setLiveIsHover(false)}
-              >
-                Live demo
-                <BsWindowPlus
-                  className={styles.projectIcon}
-                  style={
-                    liveIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-            </div>
-          </div>
-        ) : null}
-        {keskonregardeIsActive ? (
-          <div className={styles.projectInfos}>
-            <div>
-              <div className={styles.topSection}>
-                <h2 className={styles.projectTitle}>Keskonregarde ?</h2>
-                <h4 className={styles.projectStack}>
-                  React.js - CSS - Node.js
-                </h4>
-              </div>
-              <p className={styles.projectDescription}>
-                is a search website that aim to help the users to find a movie
-                to watch.
-              </p>
-            </div>
-            <div className={styles.linkSection}>
-              <a
-                href="https://github.com/LightQv/keskonregarde"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setCodeIsHover(true)}
-                onMouseLeave={() => setCodeIsHover(false)}
-              >
-                Code
-                <BsCodeSlash
-                  className={styles.projectIcon}
-                  style={
-                    codeIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-              <a
-                href="https://lightqv-keskonregarde.netlify.app/"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setLiveIsHover(true)}
-                onMouseLeave={() => setLiveIsHover(false)}
-              >
-                Live demo
-                <BsWindowPlus
-                  className={styles.projectIcon}
-                  style={
-                    liveIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-            </div>
-          </div>
-        ) : null}
-        {animalzIsActive ? (
-          <div className={styles.projectInfos}>
-            <div>
-              <div className={styles.topSection}>
-                <h2 className={styles.projectTitle}>Animalz</h2>
-                <h4 className={styles.projectStack}>
-                  React.js - CSS - Node.js
-                </h4>
-              </div>
-              <p className={styles.projectDescription}>
-                is a meeting app that help travelers to find a partner.
-                Hackathon 24h proof of concept.
-              </p>
-            </div>
-            <div className={styles.linkSection}>
-              <a
-                href="https://github.com/LightQv/animalz"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setCodeIsHover(true)}
-                onMouseLeave={() => setCodeIsHover(false)}
-              >
-                Code
-                <BsCodeSlash
-                  className={styles.projectIcon}
-                  style={
-                    codeIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-            </div>
-          </div>
-        ) : null}
-        {discoveryIsActive ? (
-          <div className={styles.projectInfos}>
-            <div>
-              <div className={styles.topSection}>
-                <h2 className={styles.projectTitle}>Discovery</h2>
-                <h4 className={styles.projectStack}>
-                  React.js - Tailwind CSS - Express.js
-                </h4>
-              </div>
-              <p className={styles.projectDescription}>
-                is a Spotify client to center the most usefull datas from the
-                user account.
-              </p>
-            </div>
-            <div className={styles.linkSection}>
-              <a
-                href="https://github.com/LightQv/discovery"
-                target="_blank"
-                className={styles.projectLink}
-                onMouseEnter={() => setCodeIsHover(true)}
-                onMouseLeave={() => setCodeIsHover(false)}
-              >
-                Code
-                <BsCodeSlash
-                  className={styles.projectIcon}
-                  style={
-                    codeIsHover
-                      ? { color: "var(--white)" }
-                      : { color: "var(--black)" }
-                  }
-                />
-              </a>
-            </div>
-          </div>
-        ) : null}
+        )}
       </section>
-      <ProjectCard
-        wildIsActive={wildIsActive}
-        pokedexIsActive={pokedexIsActive}
-        keskonregardeIsActive={keskonregardeIsActive}
-        animalzIsActive={animalzIsActive}
-        discoveryIsActive={discoveryIsActive}
-        setCardIsActive={setCardIsActive}
-        setWildIsActive={setWildIsActive}
-        setPokedexIsActive={setPokedexIsActive}
-        setKeskonregardeIsActive={setKeskonregardeIsActive}
-        setAnimalzIsActive={setAnimalzIsActive}
-        setDiscoveryIsActive={setDiscoveryIsActive}
-      />
+      <ul className={styles.projectCardBox}>
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            project={project}
+            index={index}
+            projectIsActive={projectIsActive}
+            setProjectIsActive={setProjectIsActive}
+          />
+        ))}
+      </ul>
     </main>
   );
 }
